980375c85619b5dfa775037467f9b047ca69e3f3
Thomas Claudel <tclaudel@z2r1p3.le-101.fr>
2019-11-25 11:14:02 +0100

    final push

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: tclaudel <tclaudel@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2019/10/22 17:52:44 by tclaudel     #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/14 14:13:34 by tclaudel    ###    #+. /#+    ###.fr     */
+/*   Created: 2019/10/22 16:16:41 by coscialp     #+#   ##    ##    #+#       */
+/*   Updated: 2019/11/19 16:52:56 by tclaudel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -24,20 +24,52 @@
 int			main(int ac, char **av)
 {
 	int		fd;
+	int		fd2;
     int     ret;
 	char	*line;
     int     count = 0;
 
 	line = NULL;
+	(void)ac;
 	fd = open(av[1], O_RDONLY);
-	while ((ret = get_next_line(fd, &line)) > 0)
-	{
-		dprintf(1, "%d line :\t\t%s\n", ret, line);
-        free(line);
-        count++;
-	}
-	dprintf(1, "%d line :\t\t%s\n", ret, line);
-	free(line);
-    printf("\nline readed : %d\n\n", count);
+	fd2 = open(av[2], O_RDONLY);
+	ret = get_next_line(fd, &line);
+	puts(line);
+    free(line);
+	ret = get_next_line(fd2, &line);
+	puts(line);
+    free(line);
+	ret = get_next_line(fd2, &line);
+	puts(line);
+    free(line);
+	ret = get_next_line(fd, &line);
+	puts(line);
+    free(line);
+	count++;
+	// while ((ret = get_next_line(fd, NULL)) > 0)
+	// {
+	// 	puts(line);
+    //     free(line);
+    //     count++;
+	// 	// printf("VALEUR DE RETOUR : %d\n", ret);
+	// }
+	// puts(line);
+	// free(line);
+    // printf("VALEUR DE RETOUR : %d\n", ret);
+    // printf("LIGNE LUE(S) : %d\n", count);
+	// // fd2 = open(av[2], O_RDONLY);
+	// // while ((ret = get_next_line(fd2, &line)) > 0)
+	// // {
+	// // 	puts(line);
+    // //     free(line);
+    // //     count++;
+	// // 	//printf("VALEUR DE RETOUR : %d\n", ret);
+	// // }
+	// // puts(line);
+	// // free(line);
+    // // printf("VALEUR DE RETOUR : %d\n", ret);
+    // // printf("LIGNE LUE(S) : %d\n", count);
 	close(fd);
+	close(fd2);
+	fclose(stdout);
 }
